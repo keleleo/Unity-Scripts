8d5204e9023e2be4d8bef30a03d61196547fcf29
keleleo <78627112+keleleo@users.noreply.github.com>
2021-02-05 22:38:29 -0300

    Added Save.cs
    Added a Save script, with a simple code to save information of different types Int, Float, String, object class.

---
+++ Save.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+//By keleleo
+public class Save
+{
+    //Save.SaveClass< Class >( object , path );
+    public static void SaveClass<T>(T data, string path)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+    //Save.LoadClass< Class >( path );
+    public static T LoadClass<T>(string path)
+    {
+        T data = Activator.CreateInstance<T>();
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            data = (T)formatter.Deserialize(stream);
+            return data;
+        }
+        else
+        {
+            Debug.LogError("File Not Found in: " + path);
+        }
+
+        return data;
+    }
+}
